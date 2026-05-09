daf2f583baade83c46e304bd685da4c3e2e83adb
67PranavSharma <pranavsharma29aug@gmail.com>
2026-05-09 15:45:15 +0530

    updated team photos

--- client/src/components/Team.jsx
+++ client/src/components/Team.jsx
@@ -4,8 +4,8 @@ import { motion } from 'framer-motion'
 import { FaLinkedin, FaInstagram, FaTwitter } from 'react-icons/fa'
 
 import team1 from '../assets/team1.jpg'
-import team2 from '../assets/team2new.jpg'
-import team3 from '../assets/team3new.jpg'
+import team2 from '../assets/teamnew2.jpg'
+import team3 from '../assets/teamnew3.jpg'
 
 const teamMembers = [
 
